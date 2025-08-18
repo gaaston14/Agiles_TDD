20ba4f3be26f7a404b0fe7b365478465c8479e28
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2025-08-18 20:46:34 -0300

    test sumo un numero y un string vacio

--- suma.py
+++ suma.py
@@ -2,5 +2,7 @@
 def sumar(cadena):
     if cadena == "":
         return 0
+    if cadena.endswith(","):
+        cadena = cadena[:-1]
     numeros = map(int, cadena.split(","))
     return sum(numeros)
