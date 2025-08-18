58c5e0815325c2315a4f0d3b7329e8d0421e1e28
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2025-08-18 20:40:35 -0300

    test valores vacios

--- suma_test.py
+++ suma_test.py
@@ -1,5 +1,10 @@
 import pytest
 from suma import sumar
+
 def test_sumo_2_string():
     resultado = sumar("2,1")
     assert resultado == 3
+
+def test_sumo_valores_vacios():
+    resultado = sumar("")
+    assert resultado == 0
