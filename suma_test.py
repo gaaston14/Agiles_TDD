9275f285b96cb371990c959baf73e774d310a4eb
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2025-08-18 20:49:10 -0300

    test sumo un nuemro entero y negativo

--- suma_test.py
+++ suma_test.py
@@ -12,3 +12,11 @@ def test_sumo_valores_vacios():
 def test_sumo_numero_y_valor_vacio():
     resultado = sumar("5,")
     assert resultado == 5
+
+def test_sumo_valor_vacio_y_numero():
+    resultado = sumar(",3")
+    assert resultado == 3
+
+def test_sumo_valor_negativo():
+    resultado = sumar("-1,2")
+    assert resultado == 1
